41ed7853dcaca51bc5d61e59bfb42b198900c740
nndakurnia <nandaka38@gmail.com>
2024-04-25 20:13:38 +0700

    initialized commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const {
+  volumeKubus,
+  luasPermukaanKubus,
+  volumeBalok,
+  luasPermukaanBalok,
+} = require("./rumusBangun");
+
+volumeKubus(3);
+luasPermukaanKubus(3);
+volumeBalok(6, 4, 3)
+luasPermukaanBalok(6, 4, 3)
